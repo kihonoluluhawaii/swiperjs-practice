b0d1b241eef52714c5011c492b98549b624b1402
kiho <kihonoluluhawaii@gmail.com>
2024-02-17 18:25:57 +0900

    -swiperjs_practice

---
+++ main.js
@@ -0,0 +1,18 @@
+let swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 20,
+    stretch: 0,
+    depth: 300,
+    modifier: 1,
+    slideShadows: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+});
